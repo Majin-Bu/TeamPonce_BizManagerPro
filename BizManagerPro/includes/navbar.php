c41135fb239833a049305e95382ad85f0e799edc
MadridRichard <20235694@s.ubaguio.edu>
2025-12-09 22:04:29 +0800

    made some slight changes

--- BizManagerPro/includes/navbar.php
+++ BizManagerPro/includes/navbar.php
@@ -28,27 +28,77 @@ $current_page = basename($_SERVER['PHP_SELF']);
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link <?php echo strpos($current_page, 'client') !== false ? 'active' : ''; ?>" href="<?php echo strpos($_SERVER['PHP_SELF'], 'modules') !== false ? '../clients/index.php' : (strpos($_SERVER['PHP_SELF'], 'dashboard') !== false ? '../modules/clients/index.php' : 'modules/clients/index.php'); ?>">
+                    <a class="nav-link <?php echo strpos($current_page, 'client') !== false ? 'active' : ''; ?>" href="<?php
+                        if (strpos($_SERVER['PHP_SELF'], 'dashboard/reports') !== false) {
+                            echo '../../modules/clients/index.php';
+                        } elseif (strpos($_SERVER['PHP_SELF'], 'modules') !== false) {
+                            echo '../clients/index.php';
+                        } elseif (strpos($_SERVER['PHP_SELF'], 'dashboard') !== false) {
+                            echo '../modules/clients/index.php';
+                        } else {
+                            echo 'modules/clients/index.php';
+                        }
+                    ?>">
                         <i class="fas fa-users"></i> Clients
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link <?php echo strpos($current_page, 'product') !== false ? 'active' : ''; ?>" href="<?php echo strpos($_SERVER['PHP_SELF'], 'modules') !== false ? '../products/index.php' : (strpos($_SERVER['PHP_SELF'], 'dashboard') !== false ? '../modules/products/index.php' : 'modules/products/index.php'); ?>">
+                    <a class="nav-link <?php echo strpos($current_page, 'product') !== false ? 'active' : ''; ?>" href="<?php
+                        if (strpos($_SERVER['PHP_SELF'], 'dashboard/reports') !== false) {
+                            echo '../../modules/products/index.php';
+                        } elseif (strpos($_SERVER['PHP_SELF'], 'modules') !== false) {
+                            echo '../products/index.php';
+                        } elseif (strpos($_SERVER['PHP_SELF'], 'dashboard') !== false) {
+                            echo '../modules/products/index.php';
+                        } else {
+                            echo 'modules/products/index.php';
+                        }
+                    ?>">
                         <i class="fas fa-box"></i> Products
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link <?php echo strpos($current_page, 'order') !== false ? 'active' : ''; ?>" href="<?php echo strpos($_SERVER['PHP_SELF'], 'modules') !== false ? '../orders/index.php' : (strpos($_SERVER['PHP_SELF'], 'dashboard') !== false ? '../modules/orders/index.php' : 'modules/orders/index.php'); ?>">
+                    <a class="nav-link <?php echo strpos($current_page, 'order') !== false ? 'active' : ''; ?>" href="<?php
+                        if (strpos($_SERVER['PHP_SELF'], 'dashboard/reports') !== false) {
+                            echo '../../modules/orders/index.php';
+                        } elseif (strpos($_SERVER['PHP_SELF'], 'modules') !== false) {
+                            echo '../orders/index.php';
+                        } elseif (strpos($_SERVER['PHP_SELF'], 'dashboard') !== false) {
+                            echo '../modules/orders/index.php';
+                        } else {
+                            echo 'modules/orders/index.php';
+                        }
+                    ?>">
                         <i class="fas fa-shopping-cart"></i> Orders
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link <?php echo strpos($current_page, 'payment') !== false ? 'active' : ''; ?>" href="<?php echo strpos($_SERVER['PHP_SELF'], 'modules') !== false ? '../payments/index.php' : (strpos($_SERVER['PHP_SELF'], 'dashboard') !== false ? '../modules/payments/index.php' : 'modules/payments/index.php'); ?>">
+                    <a class="nav-link <?php echo strpos($current_page, 'payment') !== false ? 'active' : ''; ?>" href="<?php
+                        if (strpos($_SERVER['PHP_SELF'], 'dashboard/reports') !== false) {
+                            echo '../../modules/payments/index.php';
+                        } elseif (strpos($_SERVER['PHP_SELF'], 'modules') !== false) {
+                            echo '../payments/index.php';
+                        } elseif (strpos($_SERVER['PHP_SELF'], 'dashboard') !== false) {
+                            echo '../modules/payments/index.php';
+                        } else {
+                            echo 'modules/payments/index.php';
+                        }
+                    ?>">
                         <i class="fas fa-credit-card"></i> Payments
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="<?php echo strpos($_SERVER['PHP_SELF'], 'modules') !== false ? '../../modules/users/logout.php' : (strpos($_SERVER['PHP_SELF'], 'dashboard') !== false ? '../modules/users/logout.php' : 'modules/users/logout.php'); ?>">
+                    <a class="nav-link" href="<?php
+                        if (strpos($_SERVER['PHP_SELF'], 'dashboard/reports') !== false) {
+                            echo '../../modules/users/logout.php';
+                        } elseif (strpos($_SERVER['PHP_SELF'], 'modules') !== false) {
+                            echo '../../modules/users/logout.php';
+                        } elseif (strpos($_SERVER['PHP_SELF'], 'dashboard') !== false) {
+                            echo '../modules/users/logout.php';
+                        } else {
+                            echo 'modules/users/logout.php';
+                        }
+                    ?>">
                         <i class="fas fa-sign-out-alt"></i> Logout
                     </a>
                 </li>
